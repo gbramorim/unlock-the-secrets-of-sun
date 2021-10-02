f671e9f65bf63f9256e0b90eb1c1608415dae313
Gabriel Amorim <gabriel.2019206500@unicap.br>
2021-10-02 09:43:36 -0300

    Sun

--- src/Sun/Sun.js
+++ src/Sun/Sun.js
@@ -1,19 +1,9 @@
-function Sun() {
-  function getCurrentDate() {
-    let newDate = new Date();
-    let date = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
-    let hour = `${newDate.getHours()}:${newDate.getMinutes()}`;
-    console.log(hour)
-      
-      
-    return `${hour} - ${date}/${month}/${year}`;
-  }
+import SunComponent from "./SunComponent/SunComponent";
 
+function Sun() {
   return (
-    <div className="sun-principal">
-      <h1>{getCurrentDate()}</h1>
+    <div>
+      <SunComponent />
     </div>
   );
 }
