8ff97679b00889c0745540f42145c3f4c51f36a1
Gabriel Amorim <gabriel.2019206500@unicap.br>
2021-10-01 21:50:29 -0300

    Current Date

--- src/Sun/Sun.js
+++ src/Sun/Sun.js
@@ -1,10 +1,18 @@
 function Sun() {
-    return (
-      <div className="sun-principal">
-        <h1>Sun 111</h1>
-      </div>
-    );
+  function getCurrentDate() {
+    let newDate = new Date();
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    return `${date}/${month}/${year}`;
   }
-  
-  export default Sun;
-  
+
+  return (
+    <div className="sun-principal">
+      <h1>{getCurrentDate()}</h1>
+    </div>
+  );
+}
+
+export default Sun;
