96b688a0ffbf753cb158ad1e65b4fd7fcb6fd657
Gabriel Amorim <gabriel.2019206500@unicap.br>
2021-10-03 15:28:38 -0300

    Profile Page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -7,6 +7,22 @@ const About = () => {
     <>
       <div className="profile-principal">
         <img src={Avatar} alt="Avatar" />
+        <div className="profile-inputs">
+          <span>Name</span>
+          <input type="text" placeholder=" XXXX" />
+        </div>
+        <div className="profile-inputs">
+          <span>Road, Number</span>
+          <input type="text" placeholder=" XXXX" />
+        </div>
+        <div className="profile-inputs">
+          <span>City</span>
+          <input type="text" placeholder=" XXXX" />
+        </div>
+        <div className="profile-inputs">
+          <span>State</span>
+          <input type="text" placeholder=" XXXX" />
+        </div>
       </div>
     </>
   );
