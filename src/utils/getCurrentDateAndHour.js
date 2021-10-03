31549aa4eaca8894daeee7418e189d336341cfed
Gabriel Amorim <gabriel.2019206500@unicap.br>
2021-10-03 17:36:03 -0300

    ai meu deus

--- src/utils/getCurrentDateAndHour.js
+++
@@ -1,9 +0,0 @@
-export default function getCurrentDateAndHour() {
-  let newDate = new Date();
-  let date = newDate.getDate();
-  let month = newDate.getMonth() + 1;
-  let year = newDate.getFullYear();
-  let hour = `${newDate.getHours()}:${newDate.getMinutes()}`;
-
-  return `${hour} - ${date}/${month}/${year}`;
-}
