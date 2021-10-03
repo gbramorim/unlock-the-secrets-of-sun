5ced75ed5645fc704f10b6dfe6bebb19ce3f187f
Gabriel Amorim <gabriel.2019206500@unicap.br>
2021-10-03 17:34:31 -0300

    Up

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import DaylightSun from "../../assets/daylightsun.png";
+import NightSun from "../../assets/nightsun.png";
+
+import "../css/Home.css";
 
 const Home = () => {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
+  const [sun, setSun] = useState(DaylightSun);
+  const [wheter, setWheter] = useState(false);
+
+  function getHourOfDay() {
+    let newDate = new Date();
+    let hour = `${newDate.getHours()}`;
+
+    const daylight = hour > 0 && hour < 18;
+    const night = hour > 18 && hour < 0;
+
+    if (daylight) {
+      setWheter(false);
+      setSun(DaylightSun);
+      document.body.style.background = "#33A2FF";
+    }
+    if (night) {
+      setWheter(true);
+      setSun(NightSun);
+      document.body.style.background = "#242C4C";
+    }
+
+    return wheter;
+  }
+
+  useEffect(() => {
+    getHourOfDay();
+  });
+
+  if (wheter === false) {
+    return (
+      <div className="home-principal">
+        <h2>Solar Science</h2>
+        <h3>xxxxxxxxxxxxxxxx</h3>
+        <img className="img-home" src={sun} alt="#" />
+        <h3>blablabla radiação</h3>
+        <h3 style={{ fontSize: "5rem" }}>10%</h3>
+      </div>
+    );
+  } else if (wheter === true) {
+    return (
+      <div className="home-principal">
+        <h2>Solar Science</h2>
+        <img className="img-home" src={sun} alt="#" />
+        <h3>blablabla radiação</h3>
+        <h3 style={{ fontSize: "5rem" }}>10%</h3>
+      </div>
+    );
+  }
 };
 
 export default Home;
